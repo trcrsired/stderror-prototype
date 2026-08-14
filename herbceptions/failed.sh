e130be91ebd45069964f4eaa9f78259fd6286817
trcrsired <oyzawqgcfc@gmail.com>
2026-08-15 05:46:50 +0800

    add test_func

---
+++ herbceptions/failed.sh
@@ -0,0 +1,1 @@
+$HOME/toolchains_build/llvm_herbceptions/build/bin/clang++ -S test.cc  -fherbceptions -std=c++26
---
+++ herbceptions/test_func.cc
@@ -0,0 +1,22 @@
+#include"error.h"
+#include<cstdio>
+#include<cstring>
+
+void f() throws
+{
+    throw throws ::std::win32_errc::file_not_found;
+}
+
+int main()
+{
+    try
+    {
+        f();
+    }
+    catch throws(::std::error e)
+    {
+        fprintf(stderr,"%d\n%s\n",
+                e==::std::win32_errc::file_not_found,
+                strerror(static_cast<int>(e.do_to_errc())));
+    }
+}
