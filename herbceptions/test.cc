e022e1076284d62bf02a3b1a88e6ac7166fa0953
trcrsired <oyzawqgcfc@gmail.com>
2026-08-15 01:53:12 +0800

    test

---
+++ herbceptions/test.cc
@@ -0,0 +1,18 @@
+#include"error.h"
+#include"pesudo_throws.h"
+#include<cstdio>
+#include<cstring>
+
+int main()
+{
+    try
+    {
+        pesudo_throws(::std::win32_errc::file_not_found);
+    }
+    catch(::std::error const& e)
+    {
+        fprintf(stderr,"%d\n%s\n",
+                e==::std::win32_errc::file_not_found,
+                strerror(static_cast<int>(e.do_to_errc())));
+    }
+}
---
+++ herbceptions/win32_domain.cc
@@ -0,0 +1,78 @@
+#include"error.h"
+#include<cerrno>
+namespace std::error_domains
+{
+
+namespace
+{
+constinit error_domain_singleton __win32_error_domain
+{
+    .do_equivalent=[](std::size_t cd, error_domain_singleton const* otherdomain,::std::size_t othercd) noexcept
+    {
+        return __win32_error_domain.do_to_errc(cd) == otherdomain->do_to_errc(othercd);
+    },
+    .do_name=[](::std::size_t, ::std::error_reporter_encoding encoding, void* cookie, ::std::error_reporter_io_cookie_function cookfun) noexcept
+    {
+        ::std::io_scatter_t v;
+        switch(encoding)
+        {
+        case ::std::error_reporter_encoding::utf16:
+        {
+            v.base=u"win32";
+            v.len=5*sizeof(char16_t);
+        }
+        case ::std::error_reporter_encoding::utf32:
+        {
+            v.base=U"win32";
+            v.len=5*sizeof(char32_t);
+        }
+        case ::std::error_reporter_encoding::utfebcdic:
+        {
+            v.base="\xA6\x89\x95\xF3\xF2";
+            v.len=5;
+        }
+        default:
+        {
+            v.base=u8"win32";
+            v.len=5;
+        }
+        }
+        cookfun(encoding, cookie,__builtin_addressof(v),1u);
+    },
+    .do_message=[](::std::size_t, ::std::error_reporter_encoding, void*, ::std::error_reporter_io_cookie_function) noexcept
+    {
+    },
+    .do_to_errc=[](::std::size_t cd) noexcept
+    {
+        switch(static_cast<::std::win32_errc>(static_cast<::std::uint_least32_t>(cd)))
+        {
+        case ::std::win32_errc::success: 
+            return static_cast<::std::errc>(0);
+        case ::std::win32_errc::invalid_function:
+            return ::std::errc::invalid_argument;
+        case ::std::win32_errc::file_not_found:
+            return ::std::errc::no_such_file_or_directory;
+        default:
+            return ::std::errc::invalid_argument;
+        }
+    }
+#if 0
+    .do_throw_dynamic_exception=[](::std::size_t cd, ::std::dynamic_exception_abi)
+    {
+#if defined(__cpp_exceptions)
+        throw ::std::system_error(__win32_error_domain.do_to_errc(cd), ::std::generic_category());
+#else
+        __builtin_trap();
+#endif
+    }
+#endif
+};
+}
+
+extern "C"
+[[__gnu__::__weak__]]
+::std::error_domain_singleton const* __cxa_error_domain_win32() noexcept
+{
+    return __builtin_addressof(::std::error_domains::__win32_error_domain);
+}
+}
