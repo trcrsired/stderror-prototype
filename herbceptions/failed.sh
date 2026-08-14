4ab1af18102d0e71f0d17aaad0ce3a0fabe7a680
trcrsired <oyzawqgcfc@gmail.com>
2026-08-15 05:48:56 +0800

    f

--- herbceptions/failed.sh
+++ herbceptions/failed.sh
@@ -1,1 +1,2 @@
-$HOME/toolchains_build/llvm_herbceptions/build/bin/clang++ -S test.cc  -fherbceptions -std=c++26
+$HOME/toolchains_build/llvm_herbceptions/build/bin/clang++ -S test.cc  -fherbceptions -std=c++26 -O3
+$HOME/toolchains_build/llvm_herbceptions/build/bin/clang++ -S test_func.cc  -fherbceptions -std=c++26 -O3
